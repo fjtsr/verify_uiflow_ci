8f289503a3bb2cda7930e5a53c8dd7c3e33118f6
fjtsr <tashiro.atsushi2001@gmail.com>
2019-04-14 09:34:03 +0000

    Added other format file

--- bin/gendoc.sh
+++ bin/gendoc.sh
@@ -1,2 +1,4 @@
-uiflow -i ../src/test.uif -o ../doc/test.png -f png
-uiflow -i ../src/test.uif -o ../doc/test.svg -f svg
+uiflow -i ../src/test.uif -o ../docs/test.dot
+uiflow -i ../src/test.uif -o ../docs/test.png -f png
+uiflow -i ../src/test.uif -o ../docs/test.svg -f svg
+uiflow -i ../src/test.uif -o ../docs/test.json -f json
